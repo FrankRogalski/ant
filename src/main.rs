e4958d29fd69a3cf635b9588ec5279a8fb863d94
Frank Rogalski <frank.rogalski@hansa-flex.com>
2025-01-28 10:22:48 +0100

    added clap to make the program more adaptable

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
-use std::array::from_fn;
+use std::sync::LazyLock;
 
 use anyhow::{anyhow, Error, Result};
+use clap::Parser;
 use rand::Rng;
 use raylib::ffi::TraceLogLevel;
 use raylib::prelude::Image;
 use raylib::{color::Color, prelude::RaylibDraw};
-
-const SCREEN_WIDTH: i32 = 1280;
-const SCREEN_HEIGHT: i32 = 720;
-const CELL_SIZE: usize = 1;
-const WIDTH: usize = SCREEN_WIDTH as usize / CELL_SIZE;
-const HEIGHT: usize = SCREEN_HEIGHT as usize / CELL_SIZE;
-const AREA: usize = WIDTH * HEIGHT;
-const ANTS: usize = 100;
-const FPS: u32 = 120;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Cell {
@@ -79,19 +71,21 @@ impl Ant {
     fn step(&mut self) {
         let pos = self.pos as isize;
         self.pos = match self.dir {
-            Direction::Up => (pos - WIDTH as isize).rem_euclid(AREA as isize) as usize,
+            Direction::Up => {
+                (pos - GLOBALS.width as isize).rem_euclid(GLOBALS.area as isize) as usize
+            }
             Direction::Right => {
-                if (self.pos + 1) % WIDTH == 0 {
-                    self.pos + 1 - WIDTH
+                if (self.pos + 1) % GLOBALS.width == 0 {
+                    self.pos + 1 - GLOBALS.width
                 } else {
                     self.pos + 1
                 }
             }
-            Direction::Down => (self.pos + WIDTH).rem_euclid(AREA),
+            Direction::Down => (self.pos + GLOBALS.width).rem_euclid(GLOBALS.area),
             Direction::Left => {
                 let pos = pos - 1;
-                (if pos.rem_euclid(WIDTH as isize) == (WIDTH - 1) as isize {
-                    pos + WIDTH as isize
+                (if pos.rem_euclid(GLOBALS.width as isize) == (GLOBALS.width - 1) as isize {
+                    pos + GLOBALS.width as isize
                 } else {
                     pos
                 }) as usize
@@ -108,34 +102,89 @@ impl Ant {
     }
 }
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Arguments {
+    #[arg(short, long, default_value_t = 20)]
+    ants: usize,
+
+    #[arg(short, long, default_value_t = 60)]
+    fps: u32,
+
+    #[arg(long, default_value_t = 1280)]
+    screen_width: u32,
+
+    #[arg(long, default_value_t = 720)]
+    screen_height: u32,
+
+    #[arg(short, long, default_value_t = 5)]
+    cell_size: u32,
+}
+
+struct Globals {
+    ants: usize,
+    fps: u32,
+    screen_width: i32,
+    screen_height: i32,
+    cell_size: i32,
+    area: usize,
+    width: usize,
+    // height: usize,
+}
+
+static GLOBALS: LazyLock<Globals> = LazyLock::new(|| {
+    let args = Arguments::parse();
+    assert!(args.screen_width > 0);
+    assert!(args.screen_height > 0);
+    assert!(args.cell_size > 0);
+    assert!(args.ants > 0);
+    assert!(args.fps > 0);
+    assert!(args.screen_width % args.cell_size == 0);
+    assert!(args.screen_height % args.cell_size == 0);
+    let width = (args.screen_width / args.cell_size) as usize;
+    let height = (args.screen_height / args.cell_size) as usize;
+    Globals {
+        ants: args.ants,
+        fps: args.fps,
+        screen_width: args.screen_width as i32,
+        screen_height: args.screen_height as i32,
+        cell_size: args.cell_size as i32,
+        width,
+        // height,
+        area: width * height,
+    }
+});
+
 fn draw_rect(pos: usize, color: Color, image: &mut Image) {
-    let x = pos % WIDTH;
-    let y = pos / WIDTH;
+    let x = pos % GLOBALS.width;
+    let y = pos / GLOBALS.width;
     image.draw_rectangle(
-        (x * CELL_SIZE) as i32,
-        (y * CELL_SIZE) as i32,
-        CELL_SIZE as i32,
-        CELL_SIZE as i32,
+        (x * GLOBALS.cell_size as usize) as i32,
+        (y * GLOBALS.cell_size as usize) as i32,
+        GLOBALS.cell_size,
+        GLOBALS.cell_size,
         color,
     );
 }
 
 fn main() -> Result<(), Error> {
-    assert!(SCREEN_WIDTH % CELL_SIZE as i32 == 0);
-    assert!(SCREEN_HEIGHT % CELL_SIZE as i32 == 0);
-    let mut rng = rand::thread_rng();
-    let mut grid: [Cell; AREA] = [Cell::Black; AREA];
+    let mut grid: Box<[Cell]> = vec![Cell::Black; GLOBALS.area].into_boxed_slice();
     let (mut rl, thread) = raylib::init()
         .title("Langton's ant")
-        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .size(GLOBALS.screen_width, GLOBALS.screen_height)
         .log_level(TraceLogLevel::LOG_WARNING)
         .build();
-    let mut ants: [Ant; ANTS] = from_fn(|_| Ant {
-        pos: rng.gen_range(0..AREA),
-        dir: Direction::random(),
-    });
-    let mut image = Image::gen_image_color(SCREEN_WIDTH, SCREEN_HEIGHT, Color::BLACK);
-    rl.set_target_fps(FPS);
+
+    let mut rng = rand::thread_rng();
+    let mut ants: Box<[Ant]> = (0..GLOBALS.ants)
+        .map(|_| Ant {
+            pos: rng.gen_range(0..GLOBALS.area),
+            dir: Direction::random(),
+        })
+        .collect();
+    let mut image =
+        Image::gen_image_color(GLOBALS.screen_width, GLOBALS.screen_height, Color::BLACK);
+    rl.set_target_fps(GLOBALS.fps);
 
     while !rl.window_should_close() {
         for ant in ants.iter_mut() {
